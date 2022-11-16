cd9c357aaf8ac574e3dbae6e34e7c22a2b9df278
Julia Scott Immer <92125966+Julia-Immer@users.noreply.github.com>
2022-11-15 22:18:02 -0700

    finished RomanToInt

--- java/RomanToInt.java
+++ java/RomanToInt.java
@@ -8,30 +8,47 @@
  * https://leetcode.com/problems/roman-to-integer/
  */
 
-import java.util.Map;
+import java.util.HashMap;
 
 class RomanToInt {
-    public int romanToInt(String s) {
-        int sum = 0;
-        Map<String, Integer> romanNums = Map.of("I", 1, "V", 5, "X", 10, "L", 50, "C", 100, "D", 500, "M", 1000);
+    public static void main(String[] args) {
+        String rNums1 = "XXVII";
+        System.out.println("Test Value:" + rNums1 
+                        + "\nExpected: 27" 
+                        + "\nromanToInt:" + romanToInt(rNums1));
 
-        if (s.length() > 0) {
-            // iterate from left to right
-            for (int i = 0; i < s.length()-1; i++) {
-                //assert romanNums.get(s.substring(i)) instanceof Integer;
-                //assert romanNums.get(s.substring(i-1)) instanceof Integer; 
-                int curr_num = romanNums.get(s.substring(i));
-                int next_num = romanNums.get(s.substring(i-1));
-                // if this char, s[i] > s[i+1], add it to the sum
-                if (curr_num <= next_num)
-                    sum += curr_num;
-                else 
-                    sum -= curr_num; // if s[i] > s[i-1], subtract it from the bunch
-            }
-
-            sum += romanNums.get(s.substring(-1));  //add very last value in
-        }
-        
-        return sum;
+        String rNums2 = "MCMXCIV";
+        System.out.println("Test Value:" + rNums2 
+                        + "\nExpected: 1994" 
+                        + "\nromanToInt:" + romanToInt(rNums2));
     }
+
+    public static int romanToInt(String s) {
+            int sum = 0;
+            HashMap<String, Integer> romanNums = new HashMap();
+            romanNums.put("I", 1);
+            romanNums.put("V", 5);
+            romanNums.put("X", 10);
+            romanNums.put("L", 50);
+            romanNums.put("C", 100);
+            romanNums.put("D", 500);
+            romanNums.put("M", 1000);
+    
+            if (s.length() > 0) {
+                // iterate from left to right
+                for (int i = 0; i < s.length()-1; i++) {
+                    int curr_num = romanNums.get(s.substring(i, i+1));
+                    int next_num = romanNums.get(s.substring(i+1, i+2));
+                    // if this char, s[i] > s[i+1], add it to the sum
+                    if (curr_num >= next_num)
+                        sum += curr_num;
+                    else 
+                        sum -= curr_num;
+                }
+    
+                sum += romanNums.get(s.substring(s.length()-1));  //add very last value in
+            }
+            
+            return sum;
+        }
 }
